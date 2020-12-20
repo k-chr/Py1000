9fca80c9bf7230707ad77f6e918aaf76412e7a60
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-12-21 00:22:25 +0100

    Refactored configdialog module

--- ui/dialogs/configdialog.py
+++ ui/dialogs/configdialog.py
@@ -1,84 +1,95 @@
 from . import (QDialog, QGraphicsDropShadowEffect,
-               QWidget, QPalette, Qt, ConfigButton, Config, BlankBg,
+               QWidget, Qt, ConfigButton, Config, BlankBg,
                QEvent, QPainter, QPaintEvent, QPushButton,
-               QVBoxLayout, QColor, QSize, QLabel, QBrush, 
-               QFont, QButtonGroup, QGroupBox, QSizePolicy,
+               QVBoxLayout, QColor, QSize, QLabel, 
+               QButtonGroup, QGroupBox, QSizePolicy,
                QHBoxLayout, Background, ConfigComboBox)
-
+from typing import Tuple
 
 class ConfigDialog(QDialog):
-
-    __preview_size = QSize(540, 320)
 
     def closeEvent(self, event: QEvent):
         event.ignore()
 
     def updatePreview(self, key):
-        self.preview.setPixmap(Config.get_instance().get_background_by_name(key).scaled(ConfigDialog.__preview_size, Qt.KeepAspectRatio))
+        self.preview.setPixmap(Config.get_instance().get_background_by_name(key))
 
     def accept(self):
         Config.get_instance().set_background_config(self.combobox.currentText())
         super(ConfigDialog, self).accept()
 
     def handle_buttons(self, button: QPushButton):
-        print(button)
+       
         if(button.text() == "OK"):
             self.accept()
         elif(button.text() == "CANCEL"):
             self.done(QDialog.Rejected)
 
-    def __init__(self, parent: QWidget =None):
-        super(ConfigDialog, self).__init__(parent)
-        config = Config.get_instance()
-        self.combobox = ConfigComboBox()
+    def __set_flags(self):
         self.setAttribute(Qt.WA_OpaquePaintEvent)
         self.setAttribute(Qt.WA_TranslucentBackground)
-
-        for key in Config.OPTIONS:
-            self.combobox.addItem(key)
-
         self.setWindowFlags(Qt.FramelessWindowHint|Qt.Tool)
 
+    def __set_shadow(self):
         shadow = QGraphicsDropShadowEffect(self)
         shadow.setBlurRadius(10)
         shadow.setColor(QColor("Black"))
         shadow.setOffset(0,0)
         self.setGraphicsEffect(shadow)
 
-        self.combobox.setCurrentText(Config.get_option_key(config.get_name()))
-        self.preview = Background(config.get_background_config(), ConfigDialog.__preview_size.height(), ConfigDialog.__preview_size.width())
-        self.layout = QVBoxLayout()
-        lab = QLabel('Choose background from comboBox')
+    def __create_label(text: str) -> QLabel:
+        lab = QLabel(text)
         lab.setObjectName('configDialogLabel')
-        
-        lab.setFont(QFont('KBREINDEERGAMES', 18))
         lab.setAutoFillBackground(True)
-        self.layout.addWidget(lab)
-        self.layout.addWidget(self.combobox)
-        lab2 = QLabel('Preview: ')
-        lab2.setAutoFillBackground(True)
-        lab2.setObjectName('configDialogLabel')
-        lab2.setFont(QFont('KBREINDEERGAMES', 18))
-        self.layout.addWidget(lab2)
-        self.layout.addWidget(self.preview, 0, Qt.AlignCenter)
-        self.combobox.currentTextChanged.connect(self.updatePreview)
-        self.pixmap = config.get_blank_background(BlankBg.BG4)
-        self.buttons = QButtonGroup()
-        self.ok_button = ConfigButton(text="OK")
+
+        return lab
+
+    def __create_dialog_buttons() -> Tuple[QGroupBox, QButtonGroup]:
+        buttons = QButtonGroup()
+
+        ok_button = ConfigButton(text="OK")
+        cancel_button = ConfigButton(text="CANCEL")
+        
+        buttons.addButton(ok_button)
+        buttons.addButton(cancel_button)
+
+        hb = QHBoxLayout()
+        hb.addWidget(ok_button)
+        hb.addWidget(cancel_button)
+
         group = QGroupBox()
         group.setObjectName("configDialogButtonGroup")
         group.setFlat(True)
-        self.cancel_button = ConfigButton(text="CANCEL")
-        self.buttons.addButton(self.ok_button)
-        self.buttons.addButton(self.cancel_button)
-        self.buttons.buttonPressed.connect(self.handle_buttons)
-        hb = QHBoxLayout()
-        hb.addWidget(self.ok_button)
-        hb.addWidget(self.cancel_button)
         group.setLayout(hb)
         group.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        self.setLayout(self.layout)
-        self.layout.addWidget(group, 0, Qt.AlignBottom|Qt.AlignRight)
+
+        return (group, buttons)
+
+    def __init__(self, parent: QWidget =None):
+        super(ConfigDialog, self).__init__(parent)
+        
+        self.__set_flags()
+        self.__set_shadow()
+
+        self.combobox = ConfigComboBox()
+        for key in Config.OPTIONS:
+            self.combobox.addItem(key)
+
+        self.combobox.setCurrentText(Config.get_option_key(Config.get_instance().get_name()))
+        self.preview = Background()
+        self.combobox.currentTextChanged.connect(self.updatePreview)
+        self.pixmap = Config.get_instance().get_blank_background(BlankBg.BG3)
+        group, self.__buttons = ConfigDialog.__create_dialog_buttons()
+        self.__buttons.buttonPressed.connect(self.handle_buttons)
+
+        layout = QVBoxLayout()
+        layout.addWidget(ConfigDialog.__create_label('Choose background from comboBox'))
+        layout.addWidget(self.combobox)
+        layout.addWidget(ConfigDialog.__create_label('Preview: '))
+        layout.addWidget(self.preview, 0, Qt.AlignCenter)
+        layout.addWidget(group, 0, Qt.AlignBottom|Qt.AlignRight)
+
+        self.setLayout(layout)
 
 
     def paintEvent(self, event: QPaintEvent):
@@ -93,5 +104,3 @@ class ConfigDialog(QDialog):
         dialog = ConfigDialog(parent)
         result = dialog.exec_()
         return result == QDialog.Accepted
-
-
