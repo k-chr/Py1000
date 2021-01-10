95579a0f0b4e4d1166bde03257e9dbc911acde96
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-01-10 20:07:24 +0100

    Refactored simulation, extracted some repeating code-chunks into methods or functions, increased invalid move penalty, to emphasize every fault of agent.

--- game/envs/simpletakingtricksenv.py
+++ game/envs/simpletakingtricksenv.py
@@ -1,142 +1,136 @@
-from . import Card, Suits, Cards, TakingTrickState, RandomCardGenerator, randint, GameRules, datetime, TrainingEnum
+from . import Suits, Card, TakingTrickState, RandomCardGenerator, randint, GameRules, datetime, TrainingEnum
 from ..utils.gamelogger import GameLogger
 from ..utils.csvlogger import CSVLogger
+from .env_player import EnvPlayer
+from .. import choice
 
-
-INVALID_MOVE_REWARD = -0.01
+INVALID_MOVE_REWARD = -50
 MY_CARD_REWARD = 0.1
 NOT_MY_CARD_REWARD = -MY_CARD_REWARD
-VALID_MOVE_REWARD = -INVALID_MOVE_REWARD * 50  
+VALID_MOVE_REWARD = -INVALID_MOVE_REWARD/30
+FULL_TRAINING_REWARD_FACTOR = 1
 
 class SimpleTakingTricksEnv(object):
     
     def __init__(self, flag: TrainingEnum =TrainingEnum.FULL_TRAINING):
-        self.player1 = []
-        self.player2 = []
+
         self.flag = flag
-        self.rules = GameRules()
-        self.left_stock = []
-        self.right_stock = []
+        self.__fun =  self.__full_training_step if self.flag is TrainingEnum.FULL_TRAINING else self.__valid_card_estimator_step if (
+           self.flag is TrainingEnum.PRETRAINING_VALID_CARDS) else self.__own_card_estimator_step
         self.generator = RandomCardGenerator()
         self.current_observation = {}
         self.played_cards = []
-        self.player1_tricks = []
-        self.player2_tricks = []
-        self.p1_score = 0
-        self.p2_score = 0
-        self.p1_invalid_actions = 0
-        self.p2_invalid_actions = 0
         self.file_name = f"simpletakingtricksenv_session_{datetime.now().strftime('%b_%d_%Y_%H_%M_%S')}.log"
         self.csv_rewards = f"simpletakingtricksenv_rewards_{datetime.now().strftime('%b_%d_%Y_%H_%M_%S')}.csv"
         self.csv_invalid = f"simpletakingtricksenv_invalid_{datetime.now().strftime('%b_%d_%Y_%H_%M_%S')}.csv"
         self.logger = GameLogger(self.file_name)
         self.csv_inv_log = CSVLogger(self.csv_invalid)
         self.csv_rew_log = CSVLogger(self.csv_rewards)
+        self.player1 = EnvPlayer("player1")
+        self.player2 = EnvPlayer("player2")
+        self.player_handler: EnvPlayer =None
+
+
+    def log_players_csv_info(self):
+        self.csv_inv_log.log((self.player1.invalid_actions, self.player2.invalid_actions))
+        self.csv_rew_log.log((self.player1.rewards, self.player2.rewards))
 
     def reset(self):
-        self.csv_inv_log.log((self.p1_invalid_actions, self.p2_invalid_actions))
-        self.csv_rew_log.log((self.p1_score, self.p2_score))
         player1 = randint(0, 1) == 1
         cards = self.generator.generate_stack_and_players_cards()
+        stock = choice([cards[0], cards[1]], size=1)[0]
         if player1:
-            self.player1 = cards[2]
-            self.player2 = cards[3]
+            self.player1.reset(cards[2], stock)
+            self.player2.reset(cards[3])
         else:
-            self.player1 = cards[3]
-            self.player2 = cards[2]
+            self.player1.reset(cards[3])
+            self.player2.reset(cards[2], stock)
         
-        self.left_stock = cards[0]
-        self.right_stock = cards[1]
         self.is_player1_turn = player1
-        self.p1_score = 0
-        self.p2_score = 0
-        self.p1_invalid_actions = 0
-        self.p2_invalid_actions = 0
-
         self.played_cards = []
-        self.player1_tricks = []
-        self.player2_tricks = []
-        self.current_observation = {'is_player1_turn':player1,
-                                    'data':TakingTrickState(hand_cards=self.player1 if player1 else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards,
-                                                            known_stock=self.left_stock if player1 else self.right_stock)}
+        self.current_observation = self.create_observation()
 
-        self.logger.append_to_log(f"player1_cards: {self.player1}")
-        self.logger.append_to_log(f"player2_cards: {self.player2}")
+        self.logger.append_to_log(f"player1_cards: {self.player1.hand_cards}")
+        self.logger.append_to_log(f"player2_cards: {self.player2.hand_cards}")
 
         return self.current_observation
 
+    def create_observation(self, card: Card =None, trump: Suits =Suits.NO_SUIT):
+        return {'is_player1_turn':self.is_player1_turn,
+                'data':TakingTrickState(hand_cards=self.player_handler.hand_cards,
+                                        tricks_taken_by_both_players=self.played_cards, trump=trump, 
+                                        played_card=card,
+                                        known_stock=self.player_handler.known_stock)}
+
+    @property
+    def player_handler(self):
+        return self.player1 if self.is_player1_turn else self.player2
+
+    @property
+    def done(self):
+        return len(self.played_cards) == 20
+        
+    @property
+    def opponent_handler(self):
+        return self.player2 if self.is_player1_turn else self.player1
+
     def step(self, action: int): 
-       fun = self.__full_training_step if self.flag is TrainingEnum.FULL_TRAINING else self.__valid_card_estimator_step if (
-           self.flag is TrainingEnum.PRETRAINING_VALID_CARDS) else self.__own_card_estimator_step
-       return fun(action)
+       return self.__fun(action)
 
-    def __update_rewards(self, reward):
-        if self.is_player1_turn:
-            self.p1_score += reward
-        else:
-            self.p2_score += reward
+    def __update_rewards(self, reward: float):
+        self.player_handler.rewards += reward
 
-    def __update_op_rewards(self, reward):
-        if self.is_player1_turn:
-            self.p2_score += reward
-        else:
-            self.p1_score += reward
+    def __update_op_rewards(self, reward: float):
+        self.opponent_handler.rewards += reward
 
-    def __own_card_estimator_step(self, action):
-        player = (self.player1,  self.player1_tricks, self.player2_tricks) if self.is_player1_turn else (self.player2, self.player2_tricks, self.player1_tricks)
+    def __own_card_estimator_step(self, action: int):
         reward = 0
         rewards = []
-        done = False
-        self.rules.set_card_collection(player)
-        l = [card for card in player if card.id() == action]
+        card: Card =None
+        trump: Suits =self.current_observation["data"].trump
+        opponent_card: Card =self.current_observation["data"].played_card
+        l = [card for card in self.player_handler.hand_cards if card.id() == action]
         if not any(l):
             reward = NOT_MY_CARD_REWARD
-            if self.is_player1_turn:
-                self.p1_invalid_actions += 1
-            else:
-                self.p2_invalid_actions += 1
+            self.player_handler.invalid_actions += 1
         else:
             reward = MY_CARD_REWARD
-            self.is_player1_turn = not self.is_player1_turn
             card = l[0]
-            player.remove(card)
-            
-            opponent_card = self.current_observation["data"].played_card
+            self.player_handler.hand_cards.remove(card)
+
             if opponent_card is not None:
                 self.played_cards.append(opponent_card)
                 self.played_cards.append(card)
-            self.current_observation = {'is_player1_turn':self.is_player1_turn,
-                                    'data':TakingTrickState(hand_cards=self.player1 if self.is_player1_turn else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards, trump=Suits.NO_SUIT, 
-                                                            played_card=card if opponent_card is None else None,
-                                                            known_stock=self.left_stock if self.is_player1_turn else self.right_stock)}
-        done = len(self.played_cards) == 20
-        self.__update_rewards(reward)
-        rewards = [reward]
-        return self.current_observation, rewards, done
+            else:
+                if(GameRules.has_pair(self.player_handler.hand_cards, card)):
+                    trump = card.suit
+                    self.logger.append_to_log((self.player_handler.name) + f" meld {trump.name} and got {trump.value} points")
 
-    def __full_training_step(self, action):
-        player, tricks, op_tricks = (self.player1,  self.player1_tricks, self.player2_tricks) if self.is_player1_turn else (
-            self.player2, self.player2_tricks, self.player1_tricks)
+        self.__update_rewards(reward)
+
+        if reward > 0:
+            self.is_player1_turn = not self.is_player1_turn
+            self.current_observation = self.create_observation(card=card if opponent_card is None else None, trump=trump)
+        rewards = [reward]
+        return self.current_observation, rewards, self.done
+
+    def __full_training_step(self, action: int):
         reward = 0
         rewards = []
-        done = False
-        self.rules.set_card_collection(player)
-        l = [card for card in player if card.id() == action]
+        card: Card =None
+        trump: Suits =self.current_observation["data"].trump
+        l = [card for card in self.player_handler.hand_cards if card.id() == action]
         opponent_card = self.current_observation["data"].played_card
-        if not any(l) or (opponent_card is not None and not self.rules.is_card_valid(l[0], opponent_card, self.current_observation["data"].trump)):
+
+        if not any(l) or (opponent_card is not None and not GameRules.is_card_valid(
+                self.player_handler.hand_cards, l[0], opponent_card, trump)):
             reward = INVALID_MOVE_REWARD
-            if self.is_player1_turn:
-                self.p1_invalid_actions += 1
-            else:
-                self.p2_invalid_actions += 1
-            rewards = [reward]
+            self.player_handler.invalid_actions += 1
         else:
             card = l[0]
-            player.remove(card)
-            
-            self.logger.append_to_log(("player1" if self.is_player1_turn else "player2") + " played card " + card.__str__())
+            self.player_handler.hand_cards.remove(card)
+            reward = 0
+            self.logger.append_to_log(self.player_handler.name + " played card " + card.__str__())
             
             if opponent_card is not None:
                 op_reward = 0
@@ -144,89 +138,67 @@ class SimpleTakingTricksEnv(object):
                 self.played_cards.append(opponent_card)
 
                 trick = [card, opponent_card]
-                if card > opponent_card or card.suit is self.current_observation["data"].trump and not (card.suit is opponent_card.suit):
-                    tricks.append(trick)
-                    reward = card.value.value + opponent_card.value.value
+                if GameRules.does_card_beat_opponents_one(card, opponent_card, trump):
+                    self.player_handler.tricks.append(trick)
+                    reward += (card.value.value + opponent_card.value.value)
                 else:
-                    op_tricks.append(trick)
-                    op_reward = card.value.value + opponent_card.value.value
-                done = len(self.played_cards) == 20
+                    self.opponent_handler.tricks.append(trick)
+                    op_reward = (card.value.value + opponent_card.value.value)
             
                 self.is_player1_turn = not self.is_player1_turn if op_reward > 0 else self.is_player1_turn
 
-                self.current_observation = {'is_player1_turn':self.is_player1_turn,
-                                    'data':TakingTrickState(hand_cards=self.player1 if self.is_player1_turn else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards, trump= self.current_observation["data"].trump,
-                                                            known_stock=self.left_stock if self.is_player1_turn else self.right_stock)}
+                self.current_observation = self.create_observation(trump=trump)
                 rewards = [reward,op_reward]
                 self.__update_op_rewards(op_reward)
             else:
-                trump = Suits.NO_SUIT
-                if(self.rules.has_pair(card)):
-                    reward = card.suit.value
+                if(GameRules.has_pair(self.player_handler.hand_cards, card)):
+                    reward += (card.suit.value)
                     trump = card.suit
-                    self.logger.append_to_log(("player1" if self.is_player1_turn else "player2") + f" meld {trump.name} by {card} and got {trump.value} points")
+                    self.logger.append_to_log(self.player_handler.name + f" meld {trump.name} by {card} and got {trump.value} points")
 
                 self.is_player1_turn = not self.is_player1_turn
                 rewards = [reward]
-                self.current_observation = {'is_player1_turn':self.is_player1_turn,
-                                    'data':TakingTrickState(hand_cards=self.player1 if self.is_player1_turn else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards, trump=trump, played_card=card,
-                                                            known_stock=self.left_stock if self.is_player1_turn else self.right_stock)}
+                self.current_observation = self.create_observation(trump=trump, card=card)
         self.__update_rewards(reward)
-        self.logger.append_to_log(f"player1_cards: {self.player1}")
-        self.logger.append_to_log(f"player2_cards: {self.player2}")
-        return self.current_observation, rewards, done
+        self.logger.append_to_log(f"player1_cards: {self.player1.hand_cards}")
+        self.logger.append_to_log(f"player2_cards: {self.player2.hand_cards}")
+        return self.current_observation, rewards, self.done
 
     def __valid_card_estimator_step(self, action):
-        player, tricks, op_tricks = (self.player1,  self.player1_tricks, self.player2_tricks) if self.is_player1_turn else (self.player2, self.player2_tricks, self.player1_tricks)
         reward = 0
         rewards = []
-        done = False
-        self.rules.set_card_collection(player)
-        l = [card for card in player if card.id() == action]
-        opponent_card = self.current_observation["data"].played_card
-        if not any(l) or (opponent_card is not None and not self.rules.is_card_valid(l[0], opponent_card, self.current_observation["data"].trump)):
+        l = [card for card in self.player_handler.hand_cards if card.id() == action]
+        opponent_card: Card =self.current_observation["data"].played_card
+        trump: Suits =self.current_observation["data"].trump
+        if not any(l) or (opponent_card is not None and not GameRules.is_card_valid(self.player_handler.hand_cards, l[0], opponent_card, trump)):
             reward = INVALID_MOVE_REWARD
-            if self.is_player1_turn:
-                self.p1_invalid_actions += 1
-            else:
-                self.p2_invalid_actions += 1
+            self.player_handler.invalid_actions += 1
         else:
             reward = VALID_MOVE_REWARD
             card = l[0]
-            player.remove(card)
-            self.logger.append_to_log(("player1" if self.is_player1_turn else "player2") + " played card " + card.__str__())
+            self.player_handler.hand_cards.remove(card)
+            self.logger.append_to_log(self.player_handler.name + " played card " + card.__str__())
             
             if opponent_card is not None:
                 op_reward = 0
                 trick = [card, opponent_card]
-                if card > opponent_card or card.suit is self.current_observation["data"].trump and not (card.suit is opponent_card.suit):
-                    tricks.append(trick)
+                if GameRules.does_card_beat_opponents_one(card, opponent_card, trump):
+                    self.player_handler.tricks.append(trick)
                 else:
-                    op_tricks.append(trick)
+                    self.opponent_handler.tricks.append(trick)
                     op_reward = 1
                 self.played_cards.append(card)
                 self.played_cards.append(opponent_card)
                 self.is_player1_turn = not self.is_player1_turn if op_reward > 0 else self.is_player1_turn
 
-                self.current_observation = {'is_player1_turn':self.is_player1_turn,
-                                    'data':TakingTrickState(hand_cards=self.player1 if self.is_player1_turn else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards, trump= self.current_observation["data"].trump,
-                                                            known_stock=self.left_stock if self.is_player1_turn else self.right_stock)}
+                self.current_observation = self.create_observation(trump=trump)
             else:
-                trump = Suits.NO_SUIT
-                if(self.rules.has_pair(card)):
+                if(GameRules.has_pair(self.player_handler.hand_cards, card)):
                     trump = card.suit
-                    self.logger.append_to_log(("player1" if self.is_player1_turn else "player2") + f" meld {trump.name} and got {trump.value} points")
+                    self.logger.append_to_log(self.player_handler.name + f" meld {trump.name} and got {trump.value} points")
 
                 self.is_player1_turn = not self.is_player1_turn
-                self.current_observation = {'is_player1_turn':self.is_player1_turn,
-                                    'data':TakingTrickState(hand_cards=self.player1 if self.is_player1_turn else self.player2,
-                                                            tricks_taken_by_both_players=self.played_cards, trump=trump, played_card=card,
-                                                            known_stock=self.left_stock if self.is_player1_turn else self.right_stock)}
+                self.current_observation = self.create_observation(trump=trump, card=card)
         rewards = [reward]
-        done = len(self.played_cards) == 20
-
         self.__update_rewards(reward)
-        return self.current_observation, rewards, done
+        return self.current_observation, rewards, self.done
