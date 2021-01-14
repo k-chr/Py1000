a19ed3aa6f5d87fe4cd0a96b3e2aedef0ea2b8da
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-01-14 18:14:21 +0100

    Fixed memory's produce_sample method behavior -> it returns null if experience replay buffer has not sufficient length to satisfy demanded size of computed batch.

--- game/ai/memory.py
+++ game/ai/memory.py
@@ -4,13 +4,13 @@ from . import (List, State, deque,
                NamedTuple)
 
 MAX_DATA_SIZE = 0x2000
-MIN_SIZE = 0x20
-
+MIN_SIZE_OF_BATCH = 0x20
 
 Batch = NamedTuple("BatchSAR", [('states', ndarray), ('actions', ndarray), ('rewards', ndarray)])
 
+
 def produce_sample(index: int) -> ndarray:
-    empty: ndarray = zeros(MIN_SIZE if index < MIN_SIZE else index + 1)
+    empty: ndarray = zeros(MIN_SIZE_OF_BATCH if index < MIN_SIZE_OF_BATCH else index + 1)
     empty[index] = 1
     return empty
 
@@ -32,16 +32,20 @@ class Memory:
         self.rewards_queue: Deque[float] =deque(maxsize=MAX_DATA_SIZE)
 
     def clean(self, reward_mapper: fun[[float], float] =None, rewards_mapper: fun[[List[float], List[float]]]=None):
+
         if reward_mapper is not None:
+
             for i in range(len(self.rewards)):
                 self.rewards[i] = reward_mapper(self.rewards[i])
+
         if rewards_mapper is not None:
             self.rewards = rewards_mapper(self.rewards)
 
         for i in range(len(self.states)):
             self.actions_queue.append(self.actions[i])
             self.states_queue.append(self.states[i])
-            self.rewards_queue.append(self.rewards[i] if reward_mapper is None else reward_mapper(self.rewards[i]))
+            self.rewards_queue.append(self.rewards[i])
+
         self.states: List[State] =[]
         self.actions: List[int] =[]
         self.rewards: List[float] =[]
@@ -49,13 +53,20 @@ class Memory:
     def update_last_reward(self, reward: float):
         self.rewards[len(self.rewards) - 1] += reward
 
-    def prepare_sample_batch(self, batch_size=MIN_SIZE, action_mapper: fun[[int], ndarray] =lambda x: produce_sample(x), 
+    def prepare_sample_batch(self, batch_size=MIN_SIZE_OF_BATCH, action_mapper: fun[[int], ndarray] =lambda x: produce_sample(x), 
         state_mapper: fun[[State], ndarray] =lambda s: s.to_one_hot_vec()) -> Batch:
 
-        indices: List[int] = choice(range(len(self.states_queue)), batch_size if batch_size >= MIN_SIZE else MIN_SIZE, replace=False)
+        clipped = batch_size if batch_size >= MIN_SIZE_OF_BATCH else MIN_SIZE_OF_BATCH
+
+        if(len(self.states_queue) < clipped):
+            return None
+
+        indices: List[int] = choice(range(len(self.states_queue)), clipped, replace=False)
+
         s: List[ndarray] = []
         a: List[ndarray] = []
         r: List[ndarray] = []
+
         for idx in indices:
             s.append(state_mapper(self.states_queue[idx]))
             a.append(action_mapper(self.actions_queue[idx]))
