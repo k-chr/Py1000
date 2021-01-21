fb9cae597d9f85d44e9bbcce2fec78e4798561ec
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-01-21 22:55:12 +0100

    Fixed issues with memory usage

--- game/ai/memorycluster.py
+++ game/ai/memorycluster.py
@@ -2,6 +2,7 @@ from .memory import Memory, fun, List
 from . import Dict, MIN_CARDS_IN_HAND, MAX_CARDS_IN_HAND
 from ..states.takingtrickstate import TakingTrickState
 
+SINGLE_CLUSTER_SIZE = 0x0800
 
 class TakingTrickClusterMemory(Memory):
 
@@ -9,7 +10,7 @@ class TakingTrickClusterMemory(Memory):
         self.init_queues()
 
     def init_queues(self):
-        self.cluster: Dict[int, Memory] = {i:Memory() for i in range(MIN_CARDS_IN_HAND, MAX_CARDS_IN_HAND + 1)}
+        self.cluster: Dict[int, Memory] = {i:Memory(SINGLE_CLUSTER_SIZE) for i in range(MIN_CARDS_IN_HAND, MAX_CARDS_IN_HAND + 1)}
 
     def remember_S_A_R_B(self, state: TakingTrickState, action: int, reward: float, behavior: float):
         self.cluster[len(state.hand_cards)].remember_S_A_R_B(state, action, reward, behavior)
@@ -17,3 +18,4 @@ class TakingTrickClusterMemory(Memory):
     def save_data_for_replay_and_clean_temp(self, reward_mapper: fun[[float], float], rewards_mapper: fun[[List[float]], List[float]]):
         for _, memory in self.cluster.items():
             memory.save_data_for_replay_and_clean_temp(reward_mapper, rewards_mapper)
+            
