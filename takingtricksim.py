f83e418a1338d3a9552b91ebb09551bbe116123a
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-01-24 23:33:56 +0100

    Implemented configuration dump, added new command line options: --episodes, --alpha, prepared Constraints objects to fit user cmd input

--- takingtricksim.py
+++ takingtricksim.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
+from tensorflow.python.ops.gen_array_ops import Const
 config = tf.compat.v1.ConfigProto(gpu_options = 
-                         tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8)
+                         tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.9)
 # device_count = {'GPU': 1}
 )
 config.gpu_options.allow_growth = True
@@ -9,7 +10,7 @@ tf.compat.v1.keras.backend.set_session(session)
 
 from functools import reduce
 from game.states.takingtrickstate import TakingTrickState
-from game import NetworkOutput
+from game import NetworkOutput, Constraint
 from game.envs.simpletakingtricksenv import SimpleTakingTricksEnv
 from game.ai.takingtricksagent import TakingTricksAgent
 from game.enums import TrainingEnum, NetworkMode, RewardMapperMode
@@ -20,10 +21,12 @@ from typing import List
 import signal
 import argparse
 from numpy import zeros
+from secrets import token_hex
 
 sys.setrecursionlimit(10000)
-EPISODES = 200_000
-DUMP_PERIOD = 5000
+ALPHA = Constraint[float](0.000001, 0.01, 0.0001)
+EPISODES = Constraint[int](100_000, 1_000_000, 200_000)
+DUMP_PERIOD = 500
 TRAINING_FLAG = TrainingEnum.PRETRAINING_OWN_CARDS
 REWARD_MAPPPER = RewardMapperMode.DISCOUNTED
 NETWORK = NetworkMode.SINGLE
@@ -36,11 +39,27 @@ def get_network_type(string: str) -> NetworkMode:
     except:
         raise argparse.ArgumentTypeError(f'provided network mode flag: {string} is not available')
 
+def get_episodes(string: str) -> int:
+    try:
+        val = int(string)
+        val = min(max(EPISODES.MIN, val), EPISODES.MAX)
+        return val
+    except:
+        raise argparse.ArgumentTypeError(f'provided episodes count: {string} is not correct value')
+
 def get_reward_mapper_type(string: str) -> RewardMapperMode:
     try: 
         return RewardMapperMode[string]
     except:
         raise argparse.ArgumentTypeError(f'provided reward mapper flag: {string} is not available')
+
+def get_alpha(string: str) -> float:
+    try:
+        val = float(string)
+        val = min(max(ALPHA.MIN, val), ALPHA.MAX)
+        return val
+    except:
+        raise argparse.ArgumentTypeError(f'provided alpha: {string} is not correct value')
 
 def get_date(string: str) -> datetime:
     try:
@@ -64,6 +83,10 @@ def init_parser() -> argparse.ArgumentParser:
                         help=f'available values: {NetworkMode._member_names_} and their combinations using \"|\" separator')
     parser.add_argument('--rewards-flag', type=get_reward_mapper_type, default=REWARD_MAPPPER, 
                         help=f'available values: {RewardMapperMode._member_names_}')
+    parser.add_argument('--episodes', type=get_episodes, default=EPISODES.DEFAULT, 
+                        help=f'provide number in range <{EPISODES.MIN}; {EPISODES.MAX}>')
+    parser.add_argument('--alpha', type=get_alpha, default=ALPHA.DEFAULT, 
+                        help=f'provide number in range <{ALPHA.MIN}; {ALPHA.MAX}>')
 
     def handler(message: str):
         print(message, file=sys.stderr)
@@ -83,23 +106,42 @@ def get_default_action(state: TakingTrickState) -> NetworkOutput:
 class Sim(QObject):
     finished = pyqtSignal()
 
+    def __dump_cfg(self):
+        from os import makedirs, path
+        if not path.isdir(path.join(self.session)):
+            makedirs(path.join(self.session), exist_ok=True)
+
+        with open(path.join(self.session, "config.cfg"), 'w') as file:
+            file.write(f"TRAINING_FLAG = {self.flag.name}\n")
+            file.write(f"NETWORK_MODE = {self.network_type.computed_name}\n")
+            file.write(f"REWARDS_MAPPER_TYPE = {self.reward_mapper_type.name}\n")
+            file.write(f"ALPHA = {self.alpha}\n")
+            file.write(f"EPISODES = {self.episodes}\n")
+            file.write(f"DATE = {self.date.strftime('%b_%d_%Y_%H_%M_%S')}\n")
+
+        return
+
     def __init__(self, args: argparse.Namespace, parent=None):
         super(Sim, self).__init__(parent)
-        date: datetime = args.date
-        self.flag: TrainingEnum = args.training_flag
-        self.network_type: NetworkMode = args.network_mode
-        self.reward_mapper_type: RewardMapperMode = args.rewards_flag
-        self.player1 = TakingTricksAgent(32, "player_agent", last_weights=date, flag=self.flag, alpha= 0.0001, 
+        self.date: datetime =args.date
+        self.flag: TrainingEnum =args.training_flag
+        self.network_type: NetworkMode =args.network_mode
+        self.reward_mapper_type: RewardMapperMode =args.rewards_flag
+        self.episodes: int =args.episodes
+        self.alpha: float =args.alpha
+        self.session = f"sessions\\session_{token_hex(8)}_{datetime.now().strftime('%b_%d_%Y_%H_%M_%S')}"
+        self.__dump_cfg()
+        self.player1 = TakingTricksAgent(32, "player_agent", self.session, last_weights=self.date, flag=self.flag, alpha= self.alpha, 
                                             mode=self.network_type, reward_mode=self.reward_mapper_type)
-        self.player2 = TakingTricksAgent(32, "player_agent", last_weights=date, flag=self.flag, alpha= 0.0001, 
+        self.player2 = TakingTricksAgent(32, "player_agent", self.session, last_weights=self.date, flag=self.flag, alpha= self.alpha, 
                                             mode=self.network_type, reward_mode=self.reward_mapper_type)
-        self.env = SimpleTakingTricksEnv(self.flag)
+        self.env = SimpleTakingTricksEnv(self.date, self.flag, self.session)
 
     @pyqtSlot()
     def run(self):
         episode = 0
 
-        while episode < EPISODES:
+        while episode < self.episodes:
             done = False
             obs = self.env.reset()
             steps = 0
@@ -124,7 +166,7 @@ class Sim(QObject):
                         op.memory.update_last_reward(rewards[1])
 
             self.env.log_episode_info()
-            print(f"[{episode + 1}/{EPISODES}]")
+            print(f"[{episode + 1}/{self.episodes}]")
 
             if self.flag is TrainingEnum.FULL_TRAINING:
                 self.player1.set_score(self.env.player1.score)
